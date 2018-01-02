1aa239f4a86e473f2b9d25ac456c9b1fe7d65d6b
bpraveenaero <bpraveen_aero@yahoo.co.in>
2018-01-02 20:45:10 +0530

    Add files via upload

--- read_cobalt_mesh.cpp
+++ read_cobalt_mesh.cpp
@@ -19,18 +19,28 @@ class FACE {
 public:
     FACE() {}
     ~FACE() {}
-    int Cell_Left, Cell_Right;
+    int lCell, rCell;
     int Vertex[4];
     int Type;
 };
+
+class CELL {
+      
+public:
+    CELL() {}
+    ~CELL() {}
+    int Vertex[8];
+    int Type;
+    int Face[6];
+};             
 
 class BC {
 
 public:
     BC() {}
     ~BC() {}
-    int Id;
-    string Name;
+    int Id, nSurfFaces;
+    string Name, prescBC;
 };
 
 vector<string> convert_string_to_vector(string);
@@ -40,7 +50,7 @@ int str_to_int(string);
 
 int main()
 {
-    ifstream meshFile, meshBcFile ;
+    //ifstream meshFile, meshBcFile ;
     string line;
     int nDimensions, nZones, nBoundaryPatches;
     int nVertices, nFaces, nCells, nVerticesPerFaceMax, nFacesPerCellMax;
@@ -49,11 +59,16 @@ int main()
     string mesh_file_name, bc_file_name;
     vector<string> line_vector;
     int nQuad=0, nTri=0;
+    int i=0, j=0;
+    int lcell, rcell;
+    int tri_face, quad_face, null_face, tmpf;
+    int nTetra=0, nPyra=0, nPrism=0, nHexa=0;
 
     mesh_file_name = "cobalt";
     bc_file_name = "cobalt.bc";
 
-    meshFile.open(mesh_file_name, ios::in);
+    ifstream meshFile(mesh_file_name.c_str());
+    ifstream meshBcFile(bc_file_name.c_str());
 
     if (!meshFile.is_open())
     {
@@ -75,43 +90,43 @@ int main()
 
     NODE *nodes = NULL;
     nodes = new NODE[nVertices];
-    for (int i=0 ; i < nVertices ; i++)
+    for ( i=0 ; i < nVertices ; i++)
     {
         if(!(meshFile >> nodes[i].X >> nodes[i].Y >> nodes[i].Z))
             cout << "Error in reading file" + mesh_file_name;
-
-        //cout << nodes[i].X << "\t"  << nodes[i].Y << "\t"  << nodes[i].Z << "\n" ;
     }
-    delete[] nodes;
+    cout << nodes[0].X << "\t"  << nodes[0].Y << "\t"  << nodes[0].Z << "\n" ;
+    cout << nodes[nVertices-1].X << "\t"  << nodes[nVertices-1].Y << "\t"  << nodes[nVertices-1].Z << "\n" ;
+    //delete[] nodes;
     FACE *faces = NULL;
     faces = new FACE[nFaces];
     for (int i=0 ; i < nFaces ; i++)
     {
-        meshFile >> faces[i].type;
-        if faces[i].type==3
-            nTri+=1;
-        else if faces[i].type==4
-            nQuad+=1;
-        else
-            cout << "New Face types found --> Faces have to be either Triangular/Quadrilateral";
+        meshFile >> faces[i].Type;
+        if (faces[i].Type==3) nTri+=1;
+        else if (faces[i].Type==4) nQuad+=1;
+        else cout << "New Face types found --> Faces have to be either Triangular/Quadrilateral";
         
-        for(int j=0 ; j< faces[i].type ; j++)
+        for(int j=0 ; j< faces[i].Type ; j++)
         {
             meshFile >> faces[i].Vertex[j];
         }
 
-        meshFile >> faces[i].Cell_Left >> faces[i].Cell_Right;
-        //cout << faces[i].Vertex[0] << "\t"  << faces[i].Vertex[1] << "\t"  << faces[i].Vertex[2] << "\t"  << faces[i].Vertex[3] << "\t"  << faces[i].Cell_Left << "\t" << faces[i].Cell_Right << "\n" ;
-
+        meshFile >> faces[i].lCell >> faces[i].rCell;
+        if (i%10000==0) cout << i << "\t" << faces[i].Vertex[0] << "\t"  << faces[i].Vertex[1] << "\t"  << faces[i].Vertex[2] << "\t"  << faces[i].Vertex[3] << "\t"  << faces[i].lCell << "\t" << faces[i].rCell << "\n" ;
     }
-    delete[] faces;
+    cout << faces[0].Type << "\t" << faces[0].lCell << "\t" << faces[0].rCell << "\n" ;
+    cout << faces[nFaces-1].Type << "\t" << faces[nFaces-1].lCell << "\t" << faces[nFaces-1].rCell << "\n" ;
+    cout << "Number of Triangles : " << "\t" << nTri << "\n" ;
+    cout << "Number of Quadrilaterals : " << "\t" << nQuad << "\n" ;
+    
     if (meshFile.bad())
         perror(("error while reading file " + mesh_file_name).c_str());
 
     meshFile.close();
     
 
-    meshBcFile.open(bc_file_name, ios::in);
+    //meshBcFile.open(bc_file_name, ios::in);
 
     if (!meshBcFile.is_open())
     {
@@ -119,14 +134,16 @@ int main()
         return 0;
     }
     BC *bcs = NULL;
-    bcs = new BC[2];
+    bcs = new BC[nBoundaryPatches+1];
 
-    while(getline(meshBcFile, line))
+    /*while(getline(meshBcFile, line))
     {
+        cout << "line : " << "\t" << line << "\n";         
         line_vector = convert_string_to_vector(line);
         if(line_vector.empty())
             continue;
         checkint = str_to_int( line_vector[0]);
+        cout << "checkint : " << "\t" << checkint << "\n";
         if(checkint == 0)
             continue;
         else
@@ -134,17 +151,141 @@ int main()
            bcs[bc_count].Id = checkint;
            getline(meshBcFile, line);
            bcs[bc_count].Name = line;
-           cout << bcs[bc_count].Id << "\t" << bcs[bc_count].Name << "\n";
-           cout << bc_count << "\n";
+           cout << bc_count << "\t" << bcs[bc_count].Id << "\t" << bcs[bc_count].Name << "\n";
+           bcs[bc_count].nSurfFaces=0;
            bc_count++;
         }
-
+    }*/
+    
+    getline(meshBcFile, line);
+    getline(meshBcFile, line);
+    getline(meshBcFile, line);
+    getline(meshBcFile, line);
+    
+    for( i=0; i< nBoundaryPatches; i++)
+    {
+     getline(meshBcFile, line);
+     line_vector = convert_string_to_vector(line);
+     checkint = str_to_int( line_vector[0]);
+     bcs[bc_count].Id = checkint;
+     getline(meshBcFile, line);
+     bcs[bc_count].Name = line;
+     getline(meshBcFile, line);
+     bcs[bc_count].prescBC = line;
+     bcs[bc_count].nSurfFaces=0;
+     bc_count++;
+     getline(meshBcFile, line);
+     getline(meshBcFile, line);
+     getline(meshBcFile, line);
     }
 
     meshBcFile.close();
+    
+    for(i=0; i<bc_count; i++) cout << i << "\t" << bcs[i].Id << "\t" << bcs[i].Name << "\t" << bcs[i].nSurfFaces << "\t" << "\t" << bcs[i].prescBC << "\n";
 
+    // Counting number of faces in each surface bc
+    
+    for (i=0 ; i<nFaces; i++)
+    {
+        if (faces[i].rCell<0)
+           if (abs(faces[i].rCell) <= nBoundaryPatches) bcs[abs(faces[i].rCell)-1].nSurfFaces+=1;
+           else cout << "Mesh and bc file mismatch" << "\t" << i << "\t" << faces[i].rCell << "\n" ;
+    }
+    
+    for(i=0; i<bc_count; i++) cout << i+1 << "\t" << bcs[i].Id << "\t" << bcs[i].Name << "\t" << bcs[i].nSurfFaces << "\t" << "\t" << bcs[i].prescBC << "\n";
 
-    return 0;
+    // building cell data base
+    
+    // face numbers forming a particular cell
+    
+    CELL *cells = NULL;
+    cells = new CELL[nCells];
+
+    for ( i=0 ; i < nFaces ; i++)
+    {
+     lcell=faces[i].lCell - 1;
+     for (j=0 ; j < 6 ; j++)
+     {
+      if (cells[lcell].Face[j]==0)
+      {
+         cells[lcell].Face[j]=i+1;
+         cells[lcell].Type+=1;
+         break;
+      }
+     }
+     rcell=faces[i].rCell - 1;
+     for (j=0 ; j < 6 ; j++)
+     {
+      if (rcell >= 0 && cells[rcell].Face[j]==0)
+      {
+         cells[rcell].Face[j]=i+1;
+         cells[rcell].Type+=1;
+         break;
+      }
+     }
+    } 
+
+    i=0;
+    cout << "cell info :" << "\t" << cells[i].Face[0] << "\t" << cells[i].Face[1] << "\t" << cells[i].Face[2] << "\t" << cells[i].Face[3] << "\t" << cells[i].Face[4] << "\t" << cells[i].Face[5] << "\t" << cells[42543].Face[6] << "\n";
+    
+    for ( i=0 ; i < nCells ; i++)
+    {
+     if (cells[i].Type < 4 || cells[i].Type > 6)
+     {
+      cout << "Cell type computation error" << "\n" ;
+     }
+     
+     tri_face=0;
+     quad_face=0;
+     null_face=0;
+     
+     for (j=0 ; j < 6 ; j++)
+     {
+      tmpf=cells[i].Face[j];
+      if (i == 42450) cout << tmpf << "\t" << faces[tmpf-1].Type << "\n" ;
+      if (tmpf == 0) null_face += 1;
+      else if (faces[tmpf-1].Type == 3) tri_face += 1;
+      else if (faces[tmpf-1].Type == 4) quad_face += 1;     
+      else cout << " Error in finding the face type" << "\t" << i << "\n";
+     }
+     if (i==42450) cout << tri_face << quad_face << null_face << "\n";
+     if (cells[i].Type == 4)
+     {
+      if (tri_face == 4 && quad_face == 0 && null_face == 2)
+      {
+       nTetra+=1;
+      }
+     }
+     else if (cells[i].Type == 5)
+     {
+      if (tri_face == 4 && quad_face == 1 && null_face == 1)
+      {
+       nPyra+=1;
+      }
+      else if (tri_face == 2 && quad_face == 3 && null_face == 1)
+      {
+       nPrism+=1;
+       cells[i].Type = 6;    //manual update to 6
+      }
+     }
+     else if (cells[i].Type == 6)
+     {
+      if (tri_face == 0 && quad_face == 6 && null_face == 0)
+      {
+       nHexa+=1;
+      }
+     }
+    }
+    
+    cout << "Number of Tetrahedra : " << "\t" << nTetra << "\n" ;
+    cout << "Number of Pyramid : " << "\t" << nPyra << "\n" ;
+    cout << "Number of Prisms : " << "\t" << nPrism << "\n" ;
+    cout << "Number of Hexahedra : " << "\t" << nHexa << "\n" ;
+            
+    delete[] faces;
+    
+    cin.get();
+    //return 0;
 }
 
 vector<string> convert_string_to_vector(string line)
